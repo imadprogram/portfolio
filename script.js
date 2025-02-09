be2b6696f5aa47e0f01537e5a30bd60d74341b77
imadprogram <98602726+imadprogram@users.noreply.github.com>
2025-02-09 17:55:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// JavaScript for animated text
+const wordElement = document.querySelector(".animated-text .word");
+const words = JSON.parse(wordElement.getAttribute("data-words"));
+let currentIndex = 0;
+
+function updateWord() {
+  wordElement.textContent = words[currentIndex];
+  currentIndex = (currentIndex + 1) % words.length;
+}
+
+setInterval(updateWord, 2000); // Change word every 2 seconds
+
+// Select the navbar element
+
+
+// Add a scroll event listener
+window.addEventListener("scroll", function () {
+  let navbar = document.querySelector('.navbar');
+  if (window.scrollY >  1) { // Adjust the scroll value as needed
+      navbar.classList.add("scroll"); // Add class when scrolling down
+  
+});
